d22503b10fd0987e3b19bb7cc40b2038a8bf8878
Tim Park <timsjpark@gmail.com>
2023-09-30 23:24:16 -0600

    Move osx aliases to the right directory

---
+++ osx/aliases.zsh
@@ -0,0 +1,2 @@
+alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
+alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,2 @@
 alias ls='ls -hG'
 alias grep='rg'
-
-alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
-alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
