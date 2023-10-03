4f6039977545a1035859d270f051617ef6a8e369
Tim Park <timsjpark@gmail.com>
2023-10-03 17:44:47 -0600

    Fix git completion by loading homebrew path before oh-my-zsh loads

--- git/completion.zsh
+++
@@ -1,9 +0,0 @@
-# Git branch autocomplete
-# https://www.oliverspryn.com/blog/adding-git-completion-to-zsh
-# https://github.com/git/git/blob/master/contrib/completion/git-completion.zsh
-
-zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash
-fpath=(~/.zsh $fpath)
-
-autoload -U compinit && compinit
-zmodload -i zsh/complist
