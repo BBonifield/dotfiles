84cb8d2097aa190c46160bb926d45a827edcf9b1
Tim Park <timsjpark@gmail.com>
2023-10-03 21:38:03 -0600

    Add comment about toggling hidden files in Finder

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,2 +1,3 @@
+# You can also toggle this feature with (CMD + SHIFT + .) in Finder
 alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES && killall Finder"
 alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO && killall Finder"
