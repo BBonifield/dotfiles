b935e40739ccb215dce82f44fa662e77ef2569f5
Tim Park <timsjpark@gmail.com>
2023-10-06 14:13:11 -0600

    Add dcbash alias to exec into docker

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -4,3 +4,5 @@ alias dc-test="docker-compose run web test_all"
 
 # Use 1password-enabled env vars with docker build
 alias dcbuild="op run --env-file=\"./.env\" docker-compose build"
+# Snapsheet docker dev
+alias dcbash="docker-compose exec app bash"
