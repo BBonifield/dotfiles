a485ef6a55c5c66985823fd3de4d6e7454abf1ef
Tim Park <timsjpark@gmail.com>
2023-10-09 10:55:19 -0600

    Add example of using 1password-enabled env in docker build

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,6 @@
 alias dc="docker-compose"
 alias dc-rspec="docker-compose run web rspec"
 alias dc-test="docker-compose run web test_all"
+
+# Use 1password-enabled env vars with docker build
+alias dcbuild="op run --env-file=\"./.env\" docker-compose build"
